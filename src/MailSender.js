1b5b36ee9a8095879f6cd3eb1dddcf014aa62bcc
sukronsabari11 <sukronsabari11@gmail.com>
2023-05-15 16:18:41 +0800

    Edit MailSender.js

--- src/MailSender.js
+++ src/MailSender.js
@@ -5,6 +5,7 @@ class MailSender {
     this._transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
